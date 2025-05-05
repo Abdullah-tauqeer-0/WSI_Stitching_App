b1f8f853b85bda1bffb4332c4f71c938d846da05
Abdullah-tauqeer-0 <tyuyhuy559@gmail.com>
2025-05-05 14:01:00 -0400

    Update test_core.py: Implement logic

---
+++ tests/test_core.py
@@ -0,0 +1,11 @@
+import unittest
+from core.utils import parse_row_col
+
+class TestUtils(unittest.TestCase):
+    def test_parse_row_col(self):
+        self.assertEqual(parse_row_col("tile_R01_C02.tif", 10), (1, 2))
+        self.assertEqual(parse_row_col("image_15_0.tif", 10), (1, 5))
+        self.assertIsNone(parse_row_col("invalid.txt", 10))
+
+if __name__ == '__main__':
+    unittest.main()
