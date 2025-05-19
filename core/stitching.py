646ca1401f686285e943ce322e67cf2bad406fc4
Abdullah-tauqeer-0 <tyuyhuy559@gmail.com>
2025-05-19 15:29:00 -0400

    Refactor stitching.py: Reviewed by AT on 2025-05-19

--- core/stitching.py
+++ core/stitching.py
@@ -284,3 +284,4 @@ def stitch_rows_iteratively(row_images: List[Tuple[int, np.ndarray]], overlap: f
 # Reviewed by AT on 2025-05-07
 # TODO: Optimize this section 80
 # TODO: Optimize this section 83
+# Reviewed by AT on 2025-05-19
